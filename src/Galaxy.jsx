2c2c0e3cdccf67d4db2f498833abad798f82c325
cameronmcnz <you@example.com>
2024-09-07 16:20:23 -0400

    's'

--- src/Galaxy.jsx
+++ src/Galaxy.jsx
@@ -16,3 +16,5 @@ export default Galaxy
 // so i was thinking of adding a section for armies down the raod a real kingdom builder.
 
 // so my first teachers never explained anything thewy tough through trying to show and shoot down and questions.
+
+// Did they play games with me i may never know...
